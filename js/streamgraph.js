df9cdd8fd8ceb91285f3c481b800db00d835a33e
statecs <statec@icloud.com>
2015-03-05 23:49:55 +0100

    Zoom & Drag

--- js/streamgraph.js
+++ js/streamgraph.js
@@ -17,6 +17,18 @@ function chart(filePath, color) {
     var margin = {top: 20, right: 40, bottom: 30, left: 30};
     var width = document.body.clientWidth - margin.left - margin.right;
     var height = 400 - margin.top - margin.bottom;
+
+    var zoom = d3.behavior.zoom()
+    .scaleExtent([1, 10])
+    .on("zoom", zoomed);
+
+    var drag = d3.behavior.drag()
+    .origin(function(d) { return d; })
+    .on("dragstart", dragstarted)
+    .on("drag", dragged)
+    .on("dragend", dragended);
+
+
     var tooltip = d3.select("body")
         .append("div")
         .attr("class", "remove")
@@ -58,11 +70,15 @@ function chart(filePath, color) {
     .x(function(d) { return x(d.date); })
     .y0(function(d) { return y(d.y0); })
     .y1(function(d) { return y(d.y0 + d.y); });
+
+
     var svg = d3.select("#chart").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(zoom);
+
     var graph = d3.csv(filePath, function(data) {
         data.forEach(function(d) {
             d.date = format.parse(d.timestamp.split(" --> ")[0]);
@@ -71,12 +87,14 @@ function chart(filePath, color) {
         var layers = stack(nest.entries(data));
         x.domain(d3.extent(data, function(d) { return d.date; }));
         y.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
+
         svg.selectAll(".layer")
             .data(layers)
             .enter().append("path")
             .attr("class", "layer")
             .attr("d", function(d) {return area(d.values); })
             .style("fill", function(d, i) { return z(i); });
+
         svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
@@ -134,7 +152,8 @@ function chart(filePath, color) {
             .style("top", "10px")
             .style("bottom", "30px")
             .style("left", "0px")
-            .style("background", "#fff");
+            .style("background", "#fff")
+                        .call(drag);
         d3.select("#chart")
             .on("mousemove", function(){  
                 mousex = d3.mouse(this);
@@ -146,4 +165,26 @@ function chart(filePath, color) {
                 vertical.style("left", mousex + "px")
             });
     });
+
+function dottype(d) {
+  d.x = +d.x;
+  d.y = +d.y;
+  return d;
+}
+function zoomed() {
+  svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+}
+
+function dragstarted(d) {
+  d3.event.sourceEvent.stopPropagation();
+  d3.select(this).classed("dragging", true);
+}
+
+function dragged(d) {
+  d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+}
+
+function dragended(d) {
+  d3.select(this).classed("dragging", false);
+}
 }

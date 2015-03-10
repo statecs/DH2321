ee36603e1b7ca812b7d381fa73d96e688fa5e8cb
Fernando Stefanini <fernando.stefanini@caelum.com.br>
2015-03-10 17:08:05 +0100

    Adding slider with mock data

--- js/progressBar.js
+++ js/progressBar.js
@@ -1,5 +1,5 @@
-function chart(filePath, color) {
-    $("#chart").empty();
+function progressBar(filePath, color) {
+    $("#progress-bar").empty();
     var datearray = [];
     var colorrange = [];
     if (color == "blue") {
@@ -16,11 +16,7 @@ function chart(filePath, color) {
     var format = d3.time.format("%I:%M:%S,%L");
     var margin = {top: 20, right: 40, bottom: 30, left: 30};
     var width = document.body.clientWidth - margin.left - margin.right;
-    var height = 400 - margin.top - margin.bottom;
-
-    var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 10])
-    .on("zoom", zoomed);
+    var height = 80 - margin.top - margin.bottom;
 
     var drag = d3.behavior.drag()
     .origin(function(d) { return d; })
@@ -29,7 +25,7 @@ function chart(filePath, color) {
     .on("dragend", dragended);
 
 
-    var tooltip = d3.select("#chart")
+    var tooltip = d3.select("#progress-bar")
         .append("div")
         .attr("class", "remove")
         .style("position", "absolute")
@@ -72,14 +68,15 @@ function chart(filePath, color) {
     .y1(function(d) { return y(d.y0 + d.y); });
 
 
-    var svg = d3.select("#chart").append("svg")
-        .style("height", height + margin.top + margin.bottom + "px")
+    var svg = d3.select("#progress-bar")
+		.style("margin-bottom", "5%")
+    	.append("svg")
         .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
         .style("position", "absolute")
         .style("left", "0")
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        // .call(zoom);
 
     var graph = d3.csv(filePath, function(data) {
         data.forEach(function(d) {
@@ -97,17 +94,6 @@ function chart(filePath, color) {
             .attr("d", function(d) {return area(d.values); })
             .style("fill", function(d, i) { return z(i); });
 
-        // svg.append("g")
-        //     .attr("class", "x axis")
-        //     .attr("transform", "translate(0," + height + ")")
-        //     .call(xAxis);
-        // svg.append("g")
-        //     .attr("class", "y axis")
-        //     .attr("transform", "translate(" + width + ", 0)");
-        //     // .call(yAxis.orient("right"));
-        // svg.append("g")
-        //         .attr("class", "y axis");
-        //         .call(yAxis.orient("left"));
         svg.selectAll(".layer")
             .attr("opacity", 1)
             .on("mouseover", function(d, i) {
@@ -121,45 +107,38 @@ function chart(filePath, color) {
                 mousex = d3.mouse(this);
                 mousex = mousex[0];
                 var invertedx = x.invert(mousex);
-                invertedx = invertedx.getHours() + ":" +invertedx.getMinutes() + ":" + invertedx.getSeconds();// + "," + invertedx.getMilliseconds();
+                invertedx = pad(invertedx.getHours()) + ":" + pad(invertedx.getMinutes()) + ":" + pad(invertedx.getSeconds());// + "," + invertedx.getMilliseconds();
                 var selected = (d.values);
-                for (var k = 0; k < selected.length; k++) {
-                    datearray[k] = selected[k].date;
-                    // datearray[k] = datearray[k].getMonth() + datearray[k].getDate();
-                    datearray[k] = datearray[k].getHours() + ":" +datearray[k].getMinutes() + ":" + datearray[k].getSeconds();// + "," + datearray[k].getMilliseconds();
-                }
-                mousedate = datearray.indexOf(invertedx);
-                if(mousedate != -1 ){
-                pro = d.values[mousedate].value;
-                set = d.values[mousedate].sentence;
                 d3.select(this)
                     .classed("hover", true)
                     .attr("stroke", strokecolor)
-                    .attr("stroke-width", "0.5px"), tooltip.html( "<p>" + d.key + "<br>" + pro + "<br/>"+set+"</p>" ).style("visibility", "visible");
-                }
+                    .attr("stroke-width", "0.5px"), tooltip.html("<p>"+invertedx+"</p>").style("visibility", "visible");
             })
             .on("mouseout", function(d, i) {
+            	mousex = d3.mouse(this);
+                mousex = mousex[0];
+                var invertedx = x.invert(mousex);
                 svg.selectAll(".layer")
                     .transition()
                     .duration(250)
                     .attr("opacity", "1");
                 d3.select(this)
                     .classed("hover", false)
-                    .attr("stroke-width", "0px"), tooltip.html( "<p>" + d.key + "<br>" + pro + "<br/>"+set+"</p>" ).style("visibility", "hidden");
+                    .attr("stroke-width", "0px"), tooltip.html("<p>"+invertedx+"</p>").style("visibility", "hidden");
             });
-        var vertical = d3.select("#chart")
+        var vertical = d3.select("#progress-bar")
             .append("div")
             .attr("class", "remove")
             .style("position", "absolute")
             .style("z-index", "19")
             .style("width", "1px")
-            .style("height", "100%")
+            .style("height", "10%")
             // .style("bottom", "300px")
             // .style("top", "0px")
             .style("left", "0px")
             .style("background", "#fff")
                         .call(drag);
-        d3.select("#chart")
+        d3.select("#progress-bar")
             .on("mousemove", function(){  
                 mousex = d3.mouse(this);
                 mousex = mousex[0] + 5;
@@ -170,6 +149,10 @@ function chart(filePath, color) {
                 vertical.style("left", mousex + "px")
             });
     });
+
+function pad(n) {
+    return (n < 10) ? ("0" + n) : n;
+}
 
 function dottype(d) {
   d.x = +d.x;
